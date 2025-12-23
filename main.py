2972af29ea33fbf760cf197691901f12dfcf13a2
Juan Daniel <juankycabj05@gmail.com>
2025-12-23 13:31:55 -0300

    db.py; main.py; requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://frontend-host.com"],  # dominio del frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Hola Render, FastAPI funcionando!"}
+    return {"message": "Backend funcionando con CORS habilitado"}
