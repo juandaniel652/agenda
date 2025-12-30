ca014ee8351acc180db898e300b10dad90579139
Juan Daniel <juankycabj05@gmail.com>
2025-12-30 16:48:41 -0300

    Health corregido para ingreso de datoa

--- app/routes/health.py
+++ app/routes/health.py
@@ -5,11 +5,15 @@ from sqlalchemy import text
 from app.dependencies import get_db
 
 router = APIRouter(
-    prefix="",
+    prefix="/health",
     tags=["Health"]
 )
 
-@router.get("/db-check")
+@router.get("/")
+def health_check():
+    return {"status": "ok"}
+
+@router.get("/db")
 def db_check(db: Session = Depends(get_db)):
     db.execute(text("SELECT 1"))
     return {
