2780a554f68774ec4409c1358ce809c98a5eb8e8
Juan Daniel <juankycabj05@gmail.com>
2026-02-16 16:45:42 -0300

    Horario base con HoraStr

--- app/schemas/horario.py
+++ app/schemas/horario.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
-from datetime import time
+from typing import Annotated
+from pydantic import BaseModel, StringConstraints
+
+HoraStr = Annotated[str, StringConstraints(pattern=r"^\d{2}:\d{2}:\d{2}$")]
 
 class HorarioBase(BaseModel):
     dia_semana: int
-    hora_inicio: time
-    hora_fin: time
+    hora_inicio: HoraStr
+    hora_fin: HoraStr
