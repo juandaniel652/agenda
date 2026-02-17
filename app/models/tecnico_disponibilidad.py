99573a013f710f668b0cb1bb04b9f672a5c93cd8
Juan Daniel <juankycabj05@gmail.com>
2026-02-17 12:37:09 -0300

    correcion a nombre de tabla

--- app/models/tecnico_disponibilidad.py
+++ app/models/tecnico_disponibilidad.py
@@ -4,7 +4,7 @@ from app.db.base import Base
 import uuid
 
 class TecnicoDisponibilidad(Base):
-    __tablename__ = "tecnicos_disponibilidad"
+    __tablename__ = "tecnico_disponibilidad"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     tecnico_id = Column(UUID(as_uuid=True), ForeignKey("tecnicos.id"), nullable=False)
